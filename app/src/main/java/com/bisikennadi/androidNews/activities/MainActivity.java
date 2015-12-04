305fe94b729233d6d7c1bdb8dcb9378e949e63ef
Bisike Nnadi <bnnadi@national.aaa.com>
2015-12-04 16:19:59 -0500

    added camera and gallery functionality, still needs toi be tested

--- app/src/main/java/com/bisikennadi/androidNews/activities/MainActivity.java
+++ app/src/main/java/com/bisikennadi/androidNews/activities/MainActivity.java
@@ -1,6 +1,15 @@
 package com.bisikennadi.androidNews.activities;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -12,10 +21,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.bisikennadi.androidNews.constants.ActivityConstant;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+    Bitmap bitmap;
+    String selectedImagePath;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -81,9 +96,13 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == com.bisikennadi.androidNews.R.id.nav_camera) {
-            // Handle the camera action
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            File f = new File(android.os.Environment.getExternalStorageDirectory(), "temp.jpg");
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+            startActivityForResult(intent, ActivityConstant.CAMERA_REQUEST);
         } else if (id == com.bisikennadi.androidNews.R.id.nav_gallery) {
-
+            Intent pictureActionIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(pictureActionIntent, ActivityConstant.GALLERY_PICTURE);
         } else if (id == com.bisikennadi.androidNews.R.id.nav_slideshow) {
 
         } else if (id == com.bisikennadi.androidNews.R.id.nav_manage) {
@@ -97,5 +116,79 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(com.bisikennadi.androidNews.R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        bitmap = null;
+        selectedImagePath = null;
+
+        if(resultCode == RESULT_OK && requestCode == ActivityConstant.CAMERA_REQUEST) {
+            File f = new File(Environment.getExternalStorageDirectory().toString());
+            for (File temp : f.listFiles()) {
+                if (temp.getName().equals("temp.jpg")) {
+                    f = temp;
+                    break;
+                }
+            }
+            if(!f.exists()) {
+                Toast.makeText(this, "Error while capturing image", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            try {
+                bitmap = BitmapFactory.decodeFile(f.getAbsolutePath());
+                bitmap = Bitmap.createScaledBitmap(bitmap,400,400,true);
+                int rotate = 0;
+                try {
+                    ExifInterface exif = new ExifInterface(f.getAbsolutePath());
+                    int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+
+                    switch (orientation) {
+                        case ExifInterface.ORIENTATION_ROTATE_270:
+                            rotate = 270;
+                            break;
+                        case ExifInterface.ORIENTATION_ROTATE_180:
+                            rotate = 180;
+                            break;
+                        case ExifInterface.ORIENTATION_ROTATE_90:
+                            rotate = 90;
+                            break;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                Matrix matrix = new Matrix();
+                matrix.postRotate(rotate);
+                bitmap = Bitmap.createBitmap(bitmap,0,0,bitmap.getWidth(),bitmap.getHeight(),matrix,true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else if(resultCode == RESULT_OK && requestCode == ActivityConstant.GALLERY_PICTURE) {
+            if(data == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show();
+            } else {
+                Uri selectedImage = data.getData();
+                String[] filePath = { MediaStore.Images.Media.DATA };
+                Cursor c = getContentResolver().query(selectedImage, filePath,
+                        null, null, null);
+                c.moveToFirst();
+                int columnIndex = c.getColumnIndex(filePath[0]);
+                selectedImagePath = c.getString(columnIndex);
+                c.close();
+
+                if (selectedImagePath != null) {
+//                    txt_image_path.setText(selectedImagePath);
+                }
+
+                bitmap = BitmapFactory.decodeFile(selectedImagePath); // load
+                // preview image
+                bitmap = Bitmap.createScaledBitmap(bitmap, 400, 400, false);
+
+
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/bisikennadi/androidNews/constants/ActivityConstant.java
@@ -0,0 +1,11 @@
+package com.bisikennadi.androidNews.constants;
+
+/**
+ * Created by BNnadi on 12/4/2015.
+ * com.bisikennadi.androidNews.constants
+ */
+public class ActivityConstant {
+    public static final int CAMERA_REQUEST = 0;
+    public static final int GALLERY_PICTURE = 1;
+
+}
